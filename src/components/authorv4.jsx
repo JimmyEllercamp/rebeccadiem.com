e0350d0163aa8b36026489c98e04c211cc3e76e8
Jimmy Ellercamp <jellercamp@timeplay.com>
2024-03-31 00:14:43 -0400

    Removed git, added gitignore and ignoring node_modules

---
+++ src/components/authorv4.jsx
@@ -0,0 +1,96 @@
+/* eslint-disable jsx-a11y/img-redundant-alt */
+/* eslint-disable jsx-a11y/anchor-is-valid */
+import GLightbox from "glightbox";
+import React, { useEffect } from "react";
+import { FiPlay } from "react-icons/fi";
+import { MdEmail } from "react-icons/md";
+import data from "../data/about.json";
+import { socialLink } from "../global";
+
+const Authorv4 = ({ isBg }) => {
+  const { aboutv4 } = data;
+  useEffect(() => {
+    GLightbox({
+      selector: ".glightbox3",
+    });
+  }, []);
+
+  return (
+    // <!-- ========== Author section start ========== -->
+    <section
+      id="author"
+      className={`section-padding authorv2 ${isBg === "yes" ? "bg-one" : ""}`}
+    >
+      <div className="container">
+        <div className="row">
+          <div className="col-xl-6 offset-xl-3 col-lg-10 offset-lg-1">
+            <div className="section-title-center text-center aboutTitle">
+              <span>{aboutv4.subtitle}</span>
+              <div className="section-divider divider-traingle"></div>
+            </div>
+          </div>
+        </div>
+        <div className="row gx-5 about-container">
+          {/* <!-- author content area start --> */}
+          <div
+            className="col-lg-6"
+            data-aos="fade-left"
+            data-aos-duration="1000"
+            data-aos-delay="200"
+          >
+            <p>{aboutv4.description}</p>
+            <p>{aboutv4.description2}</p>
+            <p>{aboutv4.description3}</p>
+            <div className="authorv2__content">
+              <ul className="social-icon mt-5">
+                {socialLink.linkedin === "" ? (
+                  ""
+                ) : (
+                  <li>
+                    <a href={socialLink.linkedin}>
+                      <img
+                        className="img-fluid"
+                        src="assets/icons/linkedin.svg"
+                        alt="icon"
+                        width="25"
+                        height="25"
+                      />
+                    </a>
+                  </li>
+                )}
+                {socialLink.instagram === "" ? (
+                  ""
+                ) : (
+                  <li>
+                    <a href={socialLink.instagram}>
+                      <img
+                        className="img-fluid instagram-icon"
+                        src="assets/icons/instagram.svg"
+                        alt="icon"
+                        width="25"
+                        height="25"
+                      />
+                    </a>
+                  </li>
+                )}
+                {socialLink.instagram === "" ? (
+                  ""
+                ) : (
+                  <li>
+                    <a href={socialLink.email}>
+                      <MdEmail className={'img-fluid'} />
+                    </a>
+                  </li>
+                )}
+              </ul>
+            </div>
+          </div>
+          {/* <!-- author content area end --> */}
+        </div>
+      </div>
+    </section>
+    // <!-- ========== Author section end ========== -->
+  );
+};
+
+export default Authorv4;
---
+++ src/global.js
@@ -0,0 +1,18 @@
+export const siteLogo = {
+    logo: "assets/images/rebecca/RebeccaDiem_Logo.png",  // Add your site logo here
+    alt: "Rebecca Diem"  // Add your site name here
+};
+export const socialLink = {
+    linkedin: "www.linkedin.com/in/rebeccadiem",
+    instagram: "https://www.instagram.com/rebeccadiem",
+    email: "mailto:rebeccadiemauthor@gmail.com",
+};
+export const mailchimpLink = {
+    link: (process.env.REACT_APP_MAILCHAIMP) // Add your mailchimp list link here
+};
+export const contactFormAction = {
+    link: (process.env.REACT_APP_CONTACTFORM)  // Add for link here from https://formspree.io
+}
+export const contactMap = {
+    link: (process.env.REACT_APP_CONTACTMAP) // Add google map link from here: https://www.embedgooglemap.net/
+}
