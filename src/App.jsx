1368d102bdd1daf01cd001ec78b5662cb7714255
Jimmy Ellercamp <jellercamp@timeplay.com>
2024-09-20 23:59:03 -0400

    Removed WOTS references in bio, added events and podcast, added favicon

--- src/App.jsx
+++ src/App.jsx
@@ -20,56 +20,80 @@ import footerData from "./data/footer.json";
 import headerData from "./data/header.json";
 
 function ScrollToTop() {
-  const { pathname } = useLocation();
-  useEffect(() => {
-    window.scrollTo(0, 0);
-  }, [pathname]);
+    const { pathname } = useLocation();
+    useEffect(() => {
+        window.scrollTo(0, 0);
+    }, [pathname]);
 
-  return null;
+    return null;
 }
 function App() {
-  const { header, headerv2, headerv3 } = headerData;
-  const { footer } = footerData;
+    const { header, headerv2, headerv3 } = headerData;
+    const { footer } = footerData;
 
-  useEffect(() => {
-    AOS.init();
-    AOS.refresh();
-  }, []);
+    useEffect(() => {
+        AOS.init();
+        AOS.refresh();
+    }, []);
 
-  useEffect(() => {
-    console.log('**** Header **** : ', header);
-  }, [header])
-
-
-  return (
-    <div className="section-wrapper">
-      <div id="preLoader"></div>
-      <BrowserRouter>
-        <ScrollToTop/>
-        <Routes>
-          <Route path="/" element={<Version04 headerv2={headerv2} footer={footer} />} />
-          <Route
-            path="single-blog"
-            element={<SingleBlog headerv3={headerv3} footer={footer} />}
-          />
-          <Route path="all-blog" element={<AllBlog headerv3={headerv3} footer={footer} />} />
-          <Route
-            path="single-event"
-            element={<SingleEvent headerv3={headerv3} footer={footer} />}
-          />
-          <Route path="all-event" element={<AllEvent headerv3={headerv3} footer={footer} />} />
-          <Route
-            path="blog-right-sidebar"
-            element={<BlogRightSidebar headerv3={headerv3} footer={footer} />}
-          />
-          <Route
-            path="blog-left-sidebar"
-            element={<BlogLeftSidebar headerv3={headerv3} footer={footer} />}
-          />
-        </Routes>
-      </BrowserRouter>
-    </div>
-  );
+    return (
+        <div className="section-wrapper">
+            <div id="preLoader"></div>
+            <BrowserRouter>
+                <ScrollToTop />
+                <Routes>
+                    <Route
+                        path="/"
+                        element={
+                            <Version04 headerv2={headerv2} footer={footer} />
+                        }
+                    />
+                    <Route
+                        path="single-blog"
+                        element={
+                            <SingleBlog headerv3={headerv3} footer={footer} />
+                        }
+                    />
+                    <Route
+                        path="all-blog"
+                        element={
+                            <AllBlog headerv3={headerv3} footer={footer} />
+                        }
+                    />
+                    <Route
+                        path="single-event"
+                        element={
+                            <SingleEvent headerv3={headerv3} footer={footer} />
+                        }
+                    />
+                    <Route
+                        path="all-event"
+                        element={
+                            <AllEvent headerv3={headerv3} footer={footer} />
+                        }
+                    />
+                    <Route
+                        path="blog-right-sidebar"
+                        element={
+                            <BlogRightSidebar
+                                headerv3={headerv3}
+                                footer={footer}
+                            />
+                        }
+                    />
+                    <Route
+                        path="blog-left-sidebar"
+                        element={
+                            <BlogLeftSidebar
+                                headerv3={headerv3}
+                                footer={footer}
+                            />
+                        }
+                    />
+                </Routes>
+            </BrowserRouter>
+        </div>
+    );
 }
 
 export default App;
--- src/global.js
+++ src/global.js
@@ -1,6 +1,6 @@
 export const siteLogo = {
-    logo: "assets/images/rebecca/RebeccaDiem_Logo.png",  // Add your site logo here
-    alt: "Rebecca Diem"  // Add your site name here
+    logo: "assets/images/rebecca/RebeccaDiem_Logo.png", // Add your site logo here
+    alt: "Rebecca Diem", // Add your site name here
 };
 export const socialLink = {
     linkedin: "www.linkedin.com/in/rebeccadiem",
@@ -8,11 +8,13 @@ export const socialLink = {
     email: "mailto:rebeccadiemauthor@gmail.com",
 };
 export const mailchimpLink = {
-    link: (process.env.REACT_APP_MAILCHAIMP) // Add your mailchimp list link here
+    link: process.env.REACT_APP_MAILCHAIMP, // Add your mailchimp list link here
 };
 export const contactFormAction = {
-    link: (process.env.REACT_APP_CONTACTFORM)  // Add for link here from https://formspree.io
-}
+    link: process.env.REACT_APP_CONTACTFORM, // Add for link here from https://formspree.io
+};
 export const contactMap = {
-    link: (process.env.REACT_APP_CONTACTMAP) // Add google map link from here: https://www.embedgooglemap.net/
-}
+    link: process.env.REACT_APP_CONTACTMAP, // Add google map link from here: https://www.embedgooglemap.net/
+};
+const favicon = document.querySelector("link[rel='icon']");
+favicon.style.filter = "invert(1)";
